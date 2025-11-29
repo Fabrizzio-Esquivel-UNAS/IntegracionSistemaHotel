09d9e76256d32838f1b4d5761ed22f28e2842374
Fabrizzio Fabiano Esquivel Mori <fabrizzio.esquivel@unas.edu.pe>
2025-11-29 14:31:41 -0500

    Fix Cycle bug

--- ProductosAPI/Program.cs
+++ ProductosAPI/Program.cs
@@ -50,7 +50,8 @@ namespace ProductosAPI
 
             // Add services to the container.
 
-            builder.Services.AddControllers();
+            builder.Services.AddControllers().AddJsonOptions(x =>
+                x.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles);
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(c =>
