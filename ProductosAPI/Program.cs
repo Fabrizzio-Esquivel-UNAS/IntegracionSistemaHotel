95337b36df15cb71cccf1d51ccb0ba5a45409a0c
DiogoAlip <diogoalipazaga@gmail.com>
2025-11-29 14:04:34 -0500

    feat: remote connection configured

--- ProductosAPI/Program.cs
+++ ProductosAPI/Program.cs
@@ -109,11 +109,8 @@ namespace ProductosAPI
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
             app.UseAuthentication();
